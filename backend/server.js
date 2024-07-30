d53b404247b9e2a2b4f4f2877435be41478f6177
Manisha <manisha30606@gmail.com>
2024-07-30 20:31:20 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
@@ -8,7 +9,7 @@ import connectMongodb from './db/connectMongodb.js';
 import cors from 'cors';
 import {app,server} from "./socket/socket.js";
 
-
+const __dirname = path.resolve();
 const corsOptions = {
     origin: 'http://localhost:3000', 
     credentials: true,
@@ -23,9 +24,9 @@ dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-    res.send('hello');
-});
+// app.get('/', (req, res) => {
+//     res.send('hello');
+// });
 
 
 
@@ -33,6 +34,11 @@ app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/users', userRoutes);
 
+app.use(express.static(path.join(__dirname,"/frontend/chat/dist")));
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist" ,"index.html"))
+})
 server.listen(PORT, async () => {
     await connectMongodb();
     console.log(`Server running on port ${PORT}`);
